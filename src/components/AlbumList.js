b329d47fc508b14b9ea3e77134a176c21d638176
Parth N Koshta <parthkoshta3@gmail.com>
2019-02-03 02:35:35 +0530

    initial

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+    return (
+        <View>
+            <Text>List</Text>
+        </View>
+    )
+}
+
+
+export default AlbumList;
