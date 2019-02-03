67b8a34d7691f91aec112d26ac07d51a4f227be2
Parth N Koshta <parthkoshta3@gmail.com>
2019-02-03 15:03:35 +0530

    app-completed

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+
+    state = { albums : [] };
+
     componentWillMount(){
         return fetch('https://rallycoding.herokuapp.com/api/music_albums')
 
         .then((response) => response.json())
 
         .then((responseJson) => {
-        console.log(responseJson[0]);
+        this.setState({ albums: responseJson });
         })
 
         .catch((error) => {
@@ -16,12 +20,19 @@ class AlbumList extends Component {
         });
     }
 
+    renderAlbums() {
+        return this.state.albums.map( album => 
+        <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
 
     render(){
+        console.log(this.state);
         return (
-            <View>
-                <Text>List!!!!</Text>
-            </View>
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
         );
     }
 
