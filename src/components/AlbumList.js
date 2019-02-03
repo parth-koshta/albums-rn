7192a38b4a49fbd6e794c2afa8561b96923de7e9
Parth N Koshta <parthkoshta3@gmail.com>
2019-02-03 10:59:31 +0530

    fetch works

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>List</Text>
-        </View>
-    )
+class AlbumList extends Component {
+    componentWillMount(){
+        return fetch('https://rallycoding.herokuapp.com/api/music_albums')
+
+        .then((response) => response.json())
+
+        .then((responseJson) => {
+        console.log(responseJson[0]);
+        })
+
+        .catch((error) => {
+        console.error(error);
+        });
+    }
+
+
+    render(){
+        return (
+            <View>
+                <Text>List!!!!</Text>
+            </View>
+        );
+    }
+
 }
 
 
